99d3188d7f5f1ed389aa3f7c66a3b88c003b6084
lethe <lethe2211@gmail.com>
2018-01-26 11:19:21 +0900

    Initial commit

--- MyMap2/ViewController.swift
+++ MyMap2/ViewController.swift
@@ -7,12 +7,14 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        inputText.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +22,37 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        
+        let searchKeyword = textField.text
+        
+        print(searchKeyword!)
+        
+        let geocoder = CLGeocoder()
+        
+        geocoder.geocodeAddressString(searchKeyword!, completionHandler: { (placemarks: [CLPlacemark]?, error: Error?) in
+            
+            if let placemark = placemarks?[0] {
+                if let targetCoordinate = placemark.location?.coordinate {
+                    print(targetCoordinate)
+                    
+                    let pin = MKPointAnnotation()
+                    pin.coordinate = targetCoordinate
+                    pin.title = searchKeyword
+                    
+                    self.displayMap.addAnnotation(pin)
+                    self.displayMap.region =
+                    MKCoordinateRegionMakeWithDistance(targetCoordinate, 500.0, 500.0)
+                }
+            }
+        })
+        
+        return true
+    }
 
+    @IBOutlet weak var inputText: UITextField!
+    
+    @IBOutlet weak var displayMap: MKMapView!
 }
 
